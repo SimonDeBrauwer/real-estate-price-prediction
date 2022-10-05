c8844810e09f2c699fd87cd63597b2a172473b55
SimonDeBrauwer <90327848+SimonDeBrauwer@users.noreply.github.com>
2022-10-05 15:05:58 +0200

    testing if I merged correctly

--- scraper.py
+++ scraper.py
@@ -1,4 +1,25 @@
+from importlib.resources import path
+from pathlib import Path
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
+path=r"C:\Users\Simon\Documents\GitHub\real-estate-price-predictions\chromedriver.exe"
+driver=webdriver.Chrome(path)
 
+
+#property listing has different sub urls than the actual property page sub url!!
+root_url="https://www.immoweb.be/en"
+property_url="/classified/apartment/for-sale/gavere/9890/10146915?searchId=633d38c4ccc41"
+listing_url="/search/apartment/for-sale?countries=BE"
+test_url=""
+driver.get({root_url, })
+print(driver.title)
+
+
+
+
+# property=requests.get(f"{root_url}{property_url}")
+
+# first_content=BeautifulSoup(property.text,'html.parser')
+# print(first_content.prettify())
